1b5fc80eae8f0f5b765236d716d534d98bb01a90
tugaykocaoglu <tugay.kocaoglu@gmail.com>
2022-05-02 01:14:44 +0300

    Async code in useEffect Hook

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+
+  // useEffect(() => {
+  //   console.log('I ONLY RUN ONCE');
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log('I RUN AFTER EVERY RENDER AND AT INITIAL RENDER');
+  // });
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get('sdsds');
+    };
+
+    search();
+  }, [term]);
 
   return (
     <div>
