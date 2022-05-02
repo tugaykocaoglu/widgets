ad435766f1e2a94dd9e4f85373cd782895f036f5
tugaykocaoglu <tugay.kocaoglu@gmail.com>
2022-05-02 21:56:59 +0300

    Added select dropdown item function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
@@ -19,17 +19,23 @@ const items = [
 ];
 
 const options = [
-  { label: 'Red', value: 'red' },
+  { label: 'The Color Red', value: 'red' },
   { label: 'The Color Green', value: 'green' },
   { label: 'A Shade of Blue', value: 'blue' },
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div className="ui container">
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
